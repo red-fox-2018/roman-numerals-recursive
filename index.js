c82f125b5a65505b2a920a67c8b16bc7a15fb841
annisaayu <annisaayu569@gmail.com>
2018-04-02 13:47:29 +0700

    roman numerals recursive

---
+++ index.js
@@ -0,0 +1,26 @@
+function toRoman(num){
+  let arabicNum = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+  let romanNum = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+  let result = '';
+
+  if(num < 1){
+    return result
+  }
+
+  for(let i = arabicNum.length -1; i >= 0; i--){
+    if(num >= arabicNum[i]){
+      result+=romanNum[i]
+      return result + toRoman(num - arabicNum[i])
+    }
+  }
+
+}
+
+console.log('My totall sweet testing script\n');
+console.log('input  | expected | actual');
+console.log('-------|----------|-------');
+console.log('4      | IV       |', toRoman(4));
+console.log('9      | IX       |', toRoman(9));
+console.log('23     | XXIII    |', toRoman(23));
+console.log('1453   | MCDLIII  |', toRoman(1453));
+console.log('1646   | MDCXLVI  |', toRoman(1646));
